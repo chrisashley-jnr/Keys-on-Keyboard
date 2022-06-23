42ba628826c0beeac3e445edb9a3706d3bb786d2
chrisashley-jnr <43844982+chrisashley-jnr@users.noreply.github.com>
2022-06-23 09:25:17 +0000

    Add files via upload

---
+++ Keyboard/main.js
@@ -0,0 +1,16 @@
+function playSound(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+    function removeTransition(e){
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
